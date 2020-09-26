809e75f39236865bf8dfe102d40b5fd4611deef4
Alex <Kazavkin@gmail.com>
2020-09-26 16:45:43 +0300

    Release 1.0.0

--- playback-authorization-redirect.py
+++ playback-authorization-redirect.py
@@ -4,41 +4,55 @@ import socketserver
 import json
 from http import server
 
+# IP и Порт сервера
 ADDR_THIS_SERVER = '141.105.134.199'
 PORT_THIS_SERVER = 8008
 
+# Группы пользователей для которых
+# не будет редиректа
 ALLOWED_USER_AGENT = [
     "allow_group"
 ]
 
+# Аддресс потока, на который будут перенаправление
 REDIRECT_LOCATION = 'http://google.com'
-ANSW_OK = '{"return_code": 200}'
 
-POST_PATHES = [
-    '/clientauth'
-]
+
+ANSW_OK = '{"return_code": 200}'
+DEBUG = True
+# Example of json request
+# {"host":"<host>", "url": "<stream URL>", "ip": "<client IP>", "user-agent": "allow_group", "referer": "< Заголовок Referer  >", "session_id": "<идентификатор сеанса Nimble>", "session_type": "<имя протокола>"}
 
 class HTTPHandler(server.BaseHTTPRequestHandler):
     __not_found = "<body>Not found</body>"
 
     def do_POST(self):
-        print('------Start handle message------')
         body = None
         content_length = self.__find_http_header(self.headers, 'Content-Length')
 
         if content_length:
-           body = self.__get_content_request_body(content_length)
+            body = self.__get_content_request_body(content_length)
+            self.____debug_('\n' + body)
 
         if self.path == '/clientauth':
             if body:
                 user_agent = self.__find_elem_in_json(body, 'user-agent')
+
                 if(user_agent):
-                    self.__controller_redirect_by_agents(ALLOWED_USER_AGENT, user_agent, self.__accept_user, self.__deny_user_redirect)     
-                    return  
+                    group_user = self.__arr_find_elem(ALLOWED_USER_AGENT, user_agent)    
+                    # Если пользователь есть в белом списке
+                    # Или перенаправление пользователя на другой поток
+                    if(group_user):
+                        self.__accept_user()
+                    elif(not group_user):
+                        self.__deny_user_redirect(REDIRECT_LOCATION)
+                else:
+                    self.__deny_user_redirect(REDIRECT_LOCATION)
+
             else:
-                self.__send_headers_403()
+                self.__send_headers(400)
         else:
-            self.__send_headers_404()
+            self.__send_headers(404, self.__not_found)
 
 
     # Нахождение заголовка зароса
@@ -53,68 +67,73 @@ class HTTPHandler(server.BaseHTTPRequestHandler):
     #Получение данных тела запроса и конвертация из байт 
     def __get_content_request_body(self, len_content):
         body_bytes = self.rfile.read(int(len_content))
-        body = body_bytes.decode()
-        return body
+        data = body_bytes.decode()
+        return data
     
 
     #Находит нужный элемент в json
-    def __find_elem_in_json(self, json, target):
+    def __find_elem_in_json(self, data, target):
         try:
-            data = json.loads(json)
+            data = json.loads(data)
             return data.get(target, '')
-        except:
-            print('-----------WARNING------------')
-            print('=====JSON is not parsable=====')
+        except json.decoder.JSONDecodeError:
+            print('\n-----------WARNING------------')
+            print('=====JSON is not parsable=====\n')
+            print('JSON: ', data)
             print('------------------------------')
             return None
 
+    def __str_to_bytrs(self, string):
+        return string.encode()
 
-    # Добавляет к запросу необходимые заголовки
-    # И отправляет ответ сервера 
-    def __send_headers_200(self, body):
-        self.send_response(200)
-        self.send_header('Content-Length', len(body))
+    def __send_headers(self, code, data = False):
+        self.send_response(code)
         self.end_headers()
-        self.wfile.write(body)
-
-    def __send_headers_404(self):
-        self.send_response(404)
-        self.send_header('Content-Length', len(self.__not_found))
-        self.end_headers()
-        self.wfile.write(self.__not_found) 
-
-    def __send_headers_403(self):
-        self.send_response(403)
-        self.send_header('Content-Length', 0)
-        self.end_headers()
+        if data:    
+            self.send_header('Content-Length', len(data))
+            self.wfile.write(self.__str_to_bytrs(data)) 
+        else:
+            self.send_header('Content-Length', 0)
 
     # Отправляет ответ на разрешение подключения к видеопотоку в nimble
     def __accept_user(self):
         answ_body = ANSW_OK
-        self.__send_headers_200(answ_body)
+        self.__send_headers(200, answ_body)
 
 
     # Отправляет ответ 200 но перенапрявляет на другой поток, указанный в константах
-    def __deny_user_redirect(self):
-        redirect_location = REDIRECT_LOCATION
+    def __deny_user_redirect(self, redirect_location):
         print('Redirect:', redirect_location)
         redirect_body = '{"return_code":302, "redirect_location":"' + redirect_location + '"}'
-        self.__send_headers_200(redirect_body)
+        self.__send_headers(200, redirect_body)
 
 
     # Нахождение из мписка разрешенных групп, нужную и принятие соответствующего решения
-    def __controller_redirect_by_agents(self, agents, user_ag, if_ok, if_deny):
+    def __controller_redirect_by_agents(self, agents, user_ag, if_ok):
         for el in agents:
             if el == user_ag:
                 if_ok()
-            return True
-        if_deny()
+                return True
+        return False
+
+    def __arr_find_elem(self, arr, element):
+        for el in arr:
+            if el == element:
+                return True
+        return False
+
+    def ____debug_(self, message):
+        if DEBUG:
+            print(message)
+
+        
 
 
 socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer((ADDR_THIS_SERVER, PORT_THIS_SERVER), HTTPHandler, True)
 
 try:
+    print('Server started at', ADDR_THIS_SERVER + ':' + PORT_THIS_SERVER)
     httpd.serve_forever()
 except KeyboardInterrupt:
     pass
