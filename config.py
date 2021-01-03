21c6481eb935eee365df8676cb1e361f28e04fc9
Alexandr Whitehorse <39910551+AlexWhitehorse@users.noreply.github.com>
2021-01-04 01:57:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 # IP и Порт сервера
-# ADDR_THIS_SERVER = '141.105.134.199'
 ADDR_THIS_SERVER = 'localhost'
 PORT_THIS_SERVER = 8008
 
@@ -10,14 +9,8 @@ ALLOWED_USER_AGENT = [
 ]
 
 WHITE_LIST_IP = [
-    '89.160.200.111',       #   AA home
-    '192.168.111.0/24',     #   internal IP Kvant
-    '10.10.0.0/16',         #   Local IP Kvant
-    '10.4.0.0/16',          #   
-    '192.168.2.0/24',       #   
-    '78.26.204.85',         #   MS home
-    '195.189.196.0/23',     #   Real IP Kvant
-    '91.202.108.137'        #   Alex home
+    'XX.XX.XX.XX', 
+    'XX.XX.XX.XX/24'
 ]
 
 # Аддресс потока, на который будут перенаправление
